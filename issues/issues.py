e7b08dfc9e17985c7cab3a15abe260be0b4dd668
Jackson <jambunjo@gmail.com>
2017-04-16 22:30:38 -0500

    Input and search

--- issues/issues.py
+++ issues/issues.py
@@ -3,15 +3,29 @@
 import MySQLdb
 
 # Opens database connection
-db = MySQLdb.connect("localhost","root","password","Metrics" )
+db = MySQLdb.connect("localhost","","","Metrics" )
 
 cursor = db.cursor()
 
+repo = raw_input('Input a Repository: ');
 
-sqlQuery = "SELECT Count(*) FROM issues"
+repoQuery = "SELECT id FROM projects where name = '%s' ORDER BY id LIMIT 1" % (repo)
 
 try:
-   cursor.execute(sql)
+   cursor.execute(repoQuery)
+   # Fetch the rows
+   results = cursor.fetchall()
+   for row in results:
+      repoId = row[0]
+      
+      print("Repo id is: %d" % (repoId) )
+except:
+   print "Error: unable to fecth data"
+
+countQuery = "SELECT count(*) FROM issues WHERE repo_id = '%s'" % (repoId)
+
+try:
+   cursor.execute(countQuery)
    # Fetch the rows
    results = cursor.fetchall()
    for row in results:
@@ -20,6 +34,7 @@ try:
       print("Number of Issues: %d" % (issueNum) )
 except:
    print "Error: unable to fecth data"
+
 
 #  disconnect from server
 db.close()
