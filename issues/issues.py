bd0bf34174d35e4a3a8a9e07d89e090688b6b83e
Jambunjo <jurrutia@loki.ist.unomaha.edu>
2017-04-19 12:37:41 -0500

    Has all metrics that are in the description

--- issues/issues.py
+++ issues/issues.py
@@ -4,7 +4,7 @@ import MySQLdb
 import sys
 
 # Opens database connection
-db = MySQLdb.connect("localhost","","","Metrics" )
+db = MySQLdb.connect("opendata.missouri.edu","msr","ghtorrent","msr" )
 
 cursor = db.cursor()
 
@@ -27,7 +27,7 @@ try:
       repoId = row[1]
       print "%s,%d" % (url, repoId)
 except:
-      print "Error: Unable to retrieve data."
+      print "Error: Unable to fetch data."
       sys.exit()
 
 
@@ -48,10 +48,11 @@ try:
       des = row[4]      
       print("Repository id: %d\ndescription: %s" % (Id,des) )
 except:
-   print "Error: unable to fecth data"
+   print "Error: unable to fetch data"
    sys.exit()
 
 countQuery = "SELECT count(*) FROM issues WHERE repo_id = '%s'" % (repoId)
+issueNum = 0
 
 try:
    cursor.execute(countQuery)
@@ -60,9 +61,9 @@ try:
    for row in results:
       issueNum = row[0]
       
-      print("Number of Issues: %d" % (issueNum) )
+      print("Number of issues: %d" % (issueNum) )
 except:
-   print "Error: unable to fecth data"
+   print "Error: unable to fetch data"
 
 
 commentQuery = "SELECT count(*) FROM issues LEFT JOIN issue_comments ON issues.id = issue_comments.issue_id WHERE issue_comments.issue_id IS NULL AND issues.repo_id = '%s';" % (repoId)
@@ -74,11 +75,11 @@ try:
    results = cursor.fetchall()
    for row in results:
       commNum = row[0]
-      print("Number of Issues without comments: %d" % (commNum) )
+      print("Number of issues without comments: %d" % (commNum) )
 except:
-   print "Error: unable to fecth data"
+   print "Error: unable to fetch data"
 
-closedQuery = "SELECT DISTINCT count(id) FROM issues INNER JOIN issue_events ON issues.id = issue_events.issue_id WHERE action = 'closed' AND issues.repo_id = '%s';" % (repoId)
+closedQuery = "SELECT DISTINCT count(id) FROM issues INNER JOIN issue_events ON issues.id = issue_events.issue_id WHERE action = 'closed'AND issues.repo_id = '%s';" % (repoId)
 
 try:
    cursor.execute(closedQuery)
@@ -86,9 +87,21 @@ try:
    results = cursor.fetchall()
    for row in results:
       closeNum = row[0]
-      print("Number of closed issues: %d" % (closeNum) )
+      print("Number of open issues: %d" % (issueNum - closeNum) )
 except:
-   print "Error: unable to fecth data"
+   print "Error: unable to fetch data"
+
+openQuery = "select count(*) from issues inner join issue_labels on issues.id = issue_labels.issue_id inner join repo_labels on issue_labels.label_id = repo_labels.id  where name = 'bug' AND issues.repo_id = '%s'" % (repoId)
+try:
+   cursor.execute(openQuery)
+   # Fetch the rows
+   results = cursor.fetchall()
+   for row in results:
+      bugNum = row[0]
+      print("Number of Bugs: %d" % ( bugNum) )
+except:
+   print "Error: unable to fetch data"
+
 
 
 #  disconnect from server
